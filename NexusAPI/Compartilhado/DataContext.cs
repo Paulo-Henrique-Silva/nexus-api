3882c0742fa97ad97845b22e3f508b1cb48da11b
Paulo <pshenrique2015@gmail.com>
2024-01-12 19:29:00 -0300

    [add]-Atualizando tabelas.

--- NexusAPI/Compartilhado/DataContext.cs
+++ NexusAPI/Compartilhado/DataContext.cs
@@ -1,9 +1,40 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NexusAPI.Administracao.Models;
+using NexusAPI.CicloVida.Models;
+using NexusAPI.Dados.Models;
 
 namespace NexusAPI.Compartilhado
 {
     public class DataContext : DbContext
     {
+        public DbSet<Notificacao> Notificacoes {  get; set; }
+
+        public DbSet<Perfil> Perfis { get; set; }
+
+        public DbSet<Projeto> Projetos { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<UsuarioProjetoPerfil> UsuarioProjetoPerfil { get; set; }
+
+        public DbSet<Atribuicao> Atribuicoes { get; set; }
+
+        public DbSet<CicloVida.Models.CicloVida> CiclosVida { get; set; }
+
+        public DbSet<CicloVidaPasso> CicloVidaPassos { get; set; }
+
+        public DbSet<Componente> Componentes {  get; set; }
+
+        public DbSet<Equipamento> Equipamentos { get; set; }
+
+        public DbSet<Localizacao> Localizacoes { get; set; }
+
+        public DbSet<Manutencao> Manutencoes { get; set; }
+
+        public DbSet<Requisicao> Requisicoes { get; set; }
+
+        public DbSet<Software> Softwares { get; set; }
+
         public DataContext(DbContextOptions options) : base(options)
         {
         }
