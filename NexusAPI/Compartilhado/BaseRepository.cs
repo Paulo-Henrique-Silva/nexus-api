83d5ca5c6fe293c3a3e9f8ea693bb939dbe391de
Paulo <pshenrique2015@gmail.com>
2024-01-13 11:00:21 -0300

    [add]-Classe Base de Repository

---
+++ NexusAPI/Compartilhado/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NexusAPI.Compartilhado
+{
+    public class BaseRepository<T> where T : ObjetoNexus
+    {
+        public DataContext DataContext { get; set; }
+
+        public BaseRepository(DataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+
+        public async Task<T?> ObterPorId(string UID)
+        {
+            return await DataContext.Set<T>()
+                .FirstAsync(obj => obj.UID == UID && obj.DataFinalizacao == null);
+        }
+
+        public async Task<List<T>> ObterTudo()
+        {
+            return await DataContext.Set<T>()
+                .Where(obj => obj.DataFinalizacao == null).ToListAsync();
+        }
+
+        public async Task<T> Adicionar(T obj)
+        {
+            await DataContext.Set<T>().AddAsync(obj);
+            await DataContext.SaveChangesAsync();
+
+            return obj;
+        }
+
+        public async Task<T> Editar(T obj)
+        {
+            DataContext.Set<T>().Update(obj);
+            await DataContext.SaveChangesAsync();
+
+            return obj;
+        }
+
+        public async Task Deletar(T obj)
+        {
+            obj.DataFinalizacao = DateTime.Now;
+            DataContext.Set<T>().Update(obj);
+            await DataContext.SaveChangesAsync();
+        }
+    }
+}
--- NexusAPI/Compartilhado/DataContext.cs
+++ NexusAPI/Compartilhado/DataContext.cs
@@ -40,8 +40,6 @@ namespace NexusAPI.Compartilhado
         {
         }
 
-
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UsuarioProjetoPerfil>()
